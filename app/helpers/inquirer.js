cea08793cae197e93b357a856b59e1ea21391426
Razon Miah <miahrazon@gmail.com>
2023-03-23 14:25:42 +0100

    implementing json db

--- app/helpers/inquirer.js
+++ app/helpers/inquirer.js
@@ -1,12 +1,15 @@
 const inquirer = require('inquirer');
-const UserController = require('../users/controller')
 const TaskController = require('../tasks/controller')
-
+const TaskRepositoryFactory = require('../tasks/repositories/task.repository.factory')
+const Task = require('../tasks/task')
 
 const {
     dbSelection,
-    firstQuestion,
-    secondQuestion
+    askName,
+    mainMenu,
+    askTitle,
+    askDetails,
+    confirmAction
 } = require('./questions')
 
 /**
@@ -17,24 +20,54 @@ async function initProgram(){
     const db = await inquirer.prompt([dbSelection])
     const dbType = db.dbType.toLowerCase();
 
-    const uc = new UserController(dbType)
-    const tc = new TaskController(dbType)
+    await listMainMenu(dbType);
 
-
-    let exit = false
-
-    const firstAnswer = await inquirer.prompt([firstQuestion])
-    const user = await uc.create({name: firstAnswer.name })
-
-    while(!exit){
-        const secondAnswer = await inquirer.prompt([secondQuestion])
-        if(secondAnswer.menu == 'Exit')
-            exit = true
-    }
-
-    
     process.exit(0)
 }
 
-module.exports = initProgram
+/**
+ * Lists main menu in terminal
+ * @param {String} dbType 
+ */
+async function listMainMenu(dbType) {
+    const tc = new TaskController(TaskRepositoryFactory.create(dbType))
+    const user = await inquirer.prompt([askName])
 
+    let exit = false;
+    while (!exit) {
+        const secondAnswer = await inquirer.prompt([mainMenu]);
+        switch (secondAnswer.menu) {
+            case 'Create a new task':
+                await createNewTask(tc, user)
+                break;
+            case 'Show tasks':
+                const t = await tc.retrieveAll()
+                console.log(t);
+                break;
+            case 'Delete tasks':
+                console.log('Delete tasks');
+                break;
+            case 'Exit':
+                exit = true;
+                break
+            default:
+                console.log('What did you selected?');
+        }
+    }
+}
+
+async function createNewTask(taskController, user) {
+    const taskTitle = await inquirer.prompt([askTitle])
+    const taskDetails = await inquirer.prompt([askDetails])
+    const ca = await inquirer.prompt([confirmAction])
+
+    if(ca.confirm){
+        await taskController.create(new Task(null, taskTitle.title, taskDetails.details, 'pending', new Date().toISOString(), null, null, user.name))
+        console.log('Task created')
+    }else {
+        console.log('Cancelled task creation')
+    }
+}
+
+
+module.exports = initProgram
--- app/tasks/controller.js
+++ app/tasks/controller.js
@@ -1,25 +1,36 @@
 const TaskSerivice = require("./service")
+const Task = require("./task")
 
 class TaskController {
-    constructor(dbType) {
-        this.service = new TaskSerivice()
-        this.type = dbType
+    constructor(repository) {
+        this.service = new TaskSerivice(repository)
     }
 
+    /**
+     * Recieves a task and calls its service to save it
+     * @param {Task} task 
+     * @returns created task
+     */
     async create(task) {
-        //TODO
+        const t = await this.service.create(task)
+        return t
     }
 
-    async showAll(){
-
-    }
-
-    async update(){
-
-    }
-
-    async delete(){
+    async retrieve(id){
         
+    }
+
+    async retrieveAll() {
+        const t = await this.service.retrtieveAll()
+        return t;
+    }
+
+    async update(task){
+
+    }
+
+    async delete(id){
+
     }
 
 
