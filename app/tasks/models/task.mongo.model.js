b2023adaf259cbdc4926cd9e58e648caa17ad260
LauraGutierrezSa <lauragsanahuja@gmail.com>
2023-03-27 10:18:50 +0200

    Modified the task.mongo.model.js file

--- app/tasks/models/task.mongo.model.js
+++ app/tasks/models/task.mongo.model.js
@@ -1,1 +1,69 @@
-//TODO
+const { MongoClient, ObjectId } = require('mongodb')
+const Task = require('../task')
+
+class TaskMongoModel {
+  constructor(uri) {
+    this.uri = uri
+    this.dbName = 'mydb'
+    this.collectionName = 'tasks'
+  }
+
+  async getCollection() {
+    const client = await MongoClient.connect(this.uri,)
+    const db = client.db(this.dbName)
+    const collection = db.collection(this.collectionName)
+    return { client, collection }
+  }
+
+  async save(task) {
+    const { client, collection } = await this.getCollection()
+    try {
+      const result = await collection.insertOne(task)
+      return result.insertedId
+    } finally {
+      client.close()
+    }
+  }
+
+  async findOne(id) {
+    const { client, collection } = await this.getCollection()
+    try {
+      const task = await collection.findOne({ _id: new ObjectId(id) })
+      return task ? new Task(task.id, task.title, task.details, task.status, task.createdAt, task.startedAt, task.finishedAt, task.createdBy) : null
+    } finally {
+      client.close()
+    }
+  }
+
+  async findAll() {
+    const { client, collection } = await this.getCollection()
+    try {
+      const tasks = await collection.find().toArray()
+      return tasks.map((task) => new Task(task.id, task.title, task.details, task.status, task.createdAt, task.startedAt, task.finishedAt, task.createdBy))
+    } finally {
+      client.close()
+    }
+  }
+
+  async delete(id) {
+    const { client, collection } = await this.getCollection()
+    try {
+      const result = await collection.deleteOne({ _id: new ObjectId(id) })
+      return result.deletedCount
+    } finally {
+      client.close()
+    }
+  }
+
+  async update(task) {
+    const { client, collection } = await this.getCollection()
+    try {
+      const result = await collection.updateOne({ _id: new ObjectId(task.id) }, { $set: task })
+      return result.modifiedCount
+    } finally {
+      client.close()
+    }
+  }
+}
+
+module.exports = TaskMongoModel
