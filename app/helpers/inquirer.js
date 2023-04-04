62b4a5993920f9843646e48511399f004c070226
LauraGutierrezSa <lauragsanahuja@gmail.com>
2023-04-04 11:34:02 +0200

    Renderized showStartedTasks function

--- app/helpers/inquirer.js
+++ app/helpers/inquirer.js
@@ -1,143 +1,142 @@
-const inquirer = require('inquirer');
-const TaskController = require('../tasks/controller')
-const TaskRepositoryFactory = require('../tasks/repositories/task.repository.factory')
-const Task = require('../tasks/task')
-
+const inquirer = require("inquirer");
+const TaskController = require("../tasks/controller");
+const TaskRepositoryFactory = require("../tasks/repositories/task.repository.factory");
+const Task = require("../tasks/task");
 
 const {
-    dbSelection,
-    askName,
-    mainMenu,
-    taskMenu,
-    askTitle,
-    askDetails,
-    confirmAction
-} = require('./questions')
+  dbSelection,
+  askName,
+  mainMenu,
+  taskMenu,
+  askTitle,
+  askDetails,
+  confirmAction,
+} = require("./questions");
 
 /**
  * Initialization of main program
  */
-async function initProgram(){
+async function initProgram() {
+  const db = await inquirer.prompt([dbSelection]);
+  const dbType = db.dbType.toLowerCase();
 
-    const db = await inquirer.prompt([dbSelection])
-    const dbType = db.dbType.toLowerCase();
+  await listMainMenu(dbType);
 
-    await listMainMenu(dbType);
-
-    process.exit(0)
+  process.exit(0);
 }
 
 /**
  * Lists main menu in terminal
- * @param {String} dbType 
+ * @param {String} dbType
  */
 async function listMainMenu(dbType) {
-    const tc = new TaskController(TaskRepositoryFactory.create(dbType))
-    const user = await inquirer.prompt([askName])
+  const tc = new TaskController(TaskRepositoryFactory.create(dbType));
+  const user = await inquirer.prompt([askName]);
 
-    let exit = false;
-    while (!exit) {
-        const menuOption = await inquirer.prompt([mainMenu]);
-        switch (menuOption.menu) {
-            case 'Create a new task':
-                await createNewTask(tc, user);
-                break;
-            case 'Show tasks':
-                await listTaskMenu();
-                break;
-            case 'Delete tasks':
-                console.log('Delete tasks');
-                break;
-            case 'Exit':
-                exit = true;
-                break
-            default:
-                console.log('What did you select?');
-        }
+  let exit = false;
+  while (!exit) {
+    const menuOption = await inquirer.prompt([mainMenu]);
+    switch (menuOption.menu) {
+      case "Create a new task":
+        await createNewTask(tc, user);
+        break;
+      case "Show tasks":
+        await listTaskMenu();
+        break;
+      case "Delete tasks":
+        console.log("Delete tasks");
+        break;
+      case "Exit":
+        exit = true;
+        break;
+      default:
+        console.log("What did you select?");
     }
+  }
 }
 
 async function listTaskMenu() {
-    let exit = false;
-    while (!exit) {
-        const menuOption = await inquirer.prompt([taskMenu]);
-        switch (menuOption.menu) {
-            case 'Pending':
-                //To-do
-                break;
-            case 'Started':
-                //To-do
-                break;
-            case 'Finished':
-                //To-do
-                break;
-            case 'Back':
-                exit = true;
-                break;
-            default:
-                console.log('What did you select?');
-        }
+  let exit = false;
+  while (!exit) {
+    const menuOption = await inquirer.prompt([taskMenu]);
+    switch (menuOption.menu) {
+      case "Pending":
+        //To-do
+        break;
+      case "Started":
+        await showStartedTasks(taskController, user);
+        break;
+      case "Finished":
+        //To-do
+        break;
+      case "Back":
+        exit = true;
+        break;
+      default:
+        console.log("What did you select?");
     }
+  }
 }
 
 async function createNewTask(taskController, user) {
-    const taskTitle = await inquirer.prompt([askTitle])
-    const taskDetails = await inquirer.prompt([askDetails])
-    const ca = await inquirer.prompt([confirmAction])
+  const taskTitle = await inquirer.prompt([askTitle]);
+  const taskDetails = await inquirer.prompt([askDetails]);
+  const ca = await inquirer.prompt([confirmAction]);
 
-    if(ca.confirm){
-        await taskController.create(new Task(null, taskTitle.title, taskDetails.details, 'pending', new Date().toISOString(), null, null, user.name))
-        console.log('Task created')
-    }else {
-        console.log('Cancelled task creation')
-    }
+  if (ca.confirm) {
+    await taskController.create(
+      new Task(
+        null,
+        taskTitle.title,
+        taskDetails.details,
+        "pending",
+        new Date().toISOString(),
+        null,
+        null,
+        user.name
+      )
+    );
+    console.log("Task created");
+  } else {
+    console.log("Cancelled task creation");
+  }
 }
-
 
 //show all started tasks:
 async function showStartedTasks(taskController, user) {
-    let exit = false;
-    const tasksArray = await taskController.getStartedTasks();
-    const tasksByUser = getTasksByUser(tasksArray, user);
-    const choices = getTaskTitlesMenu(tasksByUser, 'Select a task to view more details:');
+  let exit = false;
+  const tasksArray = await taskController.getStartedTasks();
+  const tasksByUser = getTasksByUser(tasksArray, user);
+  const menu = getTaskTitlesMenu(
+    tasksByUser,
+    "Select a task to view more details:"
+  );
 
-    while (!exit) {
-        const menuOption = await inquirer.prompt([{
-            type: 'list',
-            name: 'menu',
-            message: 'Select a task to view more details:',
-            choices: choices
-        }]);
-        if(menuOption.menu === 'Back') {
-            exit = true;
-        } else {
-            console.log(tasksByUser[(menuOption.menu.charAt(0) - 1)]);
-        }
+  while (!exit) {
+    const menuOption = await inquirer.prompt([menu]);
+    if (menuOption.menu === "Back") {
+      exit = true;
+    } else {
+      console.log(tasksByUser[menuOption.menu.charAt(0) - 1]);
     }
-}
+  }
 }
 
 function getTasksByUser(tasksArray, user) {
-    return tasksArray.filter(task => task.createdBy === user.name);
+  return tasksArray.filter((task) => task.createdBy === user.name);
 }
 
 function getTaskTitlesMenu(tasks, message) {
-    const choices = tasks.map((task, index) => `${index + 1}. ${task.title}`);
-    choices.push('Back');
-    return [{
-        type: 'list',
-        name: 'menu',
-        message: message,
-        choices: choices
-    }];
-
+  const choices = tasks.map((task, index) => `${index + 1}. ${task.title}`);
+  choices.push("Back");
+  return [
+    {
+      type: "list",
+      name: "menu",
+      message: message,
+      choices: choices,
+    },
+  ];
 }
 
-
-
-
-
-
-
-
-module.exports = initProgram
+module.exports = initProgram;
