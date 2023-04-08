197752b2ade514cf5dfd93ebfe1ff4a0bd5ebf8e
LauraGutierrezSa <lauragsanahuja@gmail.com>
2023-04-08 22:09:26 +0200

    showStartedTasks: now user can delete tasks

--- app/helpers/inquirer.js
+++ app/helpers/inquirer.js
@@ -32,7 +32,7 @@ async function initProgram() {
 async function listMainMenu(dbType) {
   const tc = new TaskController(TaskRepositoryFactory.create(dbType));
   const answer = await inquirer.prompt([askName]);
-  const user = answer.name
+  const user = answer.name;
 
   let exit = false;
   while (!exit) {
@@ -45,7 +45,7 @@ async function listMainMenu(dbType) {
         await listTaskMenu(tc, user);
         break;
       case "Delete tasks":
-        await deleteTask(tc, user)
+        await deleteTask(tc, user);
         break;
       case "Exit":
         exit = true;
@@ -56,35 +56,37 @@ async function listMainMenu(dbType) {
   }
 }
 
-async function deleteTask(taskController, user){
-    const tasksArray = await taskController.retrieveAll()
-    
-    if(tasksArray.getTasks().length){
-        const tasksByUser = getTasksByUser(tasksArray.getTasks(), user)
-        await showDeleteTasksMenu(taskController, tasksByUser)
-    }else{
-        console.log('There are no tasks to delete')
-    }
+async function deleteTask(taskController, user) {
+  const tasksArray = await taskController.retrieveAll();
+
+  if (tasksArray.getTasks().length) {
+    const tasksByUser = getTasksByUser(tasksArray.getTasks(), user);
+    await showDeleteTasksMenu(taskController, tasksByUser);
+  } else {
+    console.log("There are no tasks to delete");
+  }
 }
 
-async function showDeleteTasksMenu(taskController, tasksByUser){
-    let menu = getTaskTitlesMenu(tasksByUser, 'Select a task to delete:')
-    let exit = false;
+async function showDeleteTasksMenu(taskController, tasksByUser) {
+  let menu = getTaskTitlesMenu(tasksByUser, "Select a task to delete:");
+  let exit = false;
 
-    while (!exit) {
-        const menuOption = await inquirer.prompt([menu]);
-        if (menuOption.menu === "Back") {
-            exit = true;
-        } else {
-            const ca = await inquirer.prompt([confirmAction]);
-            if (ca.confirm) {
-                await taskController.delete((tasksByUser[menuOption.menu.charAt(0) - 1]).id)
-                tasksByUser.splice((menuOption.menu.charAt(0) - 1), 1)
-                menu = getTaskTitlesMenu(tasksByUser, 'Select a task to delete:')
-                console.log('Succesfully deleted')
-            }
-        }
+  while (!exit) {
+    const menuOption = await inquirer.prompt([menu]);
+    if (menuOption.menu === "Back") {
+      exit = true;
+    } else {
+      const ca = await inquirer.prompt([confirmAction]);
+      if (ca.confirm) {
+        await taskController.delete(
+          tasksByUser[menuOption.menu.charAt(0) - 1].id
+        );
+        tasksByUser.splice(menuOption.menu.charAt(0) - 1, 1);
+        menu = getTaskTitlesMenu(tasksByUser, "Select a task to delete:");
+        console.log("Succesfully deleted");
+      }
     }
+  }
 }
 
 async function listTaskMenu(taskController, user) {
@@ -137,26 +139,55 @@ async function createNewTask(taskController, user) {
 //show all started tasks:
 async function showStartedTasks(taskController, user) {
   let exit = false;
-  let tasksArray = await taskController.getStartedTasks();
-  let tasksByUser = getTasksByUser(tasksArray, user);
+  const tasksArray = await taskController.getStartedTasks();
+  const tasksByUser = getTasksByUser(tasksArray, user);
   const menu = getTaskTitlesMenu(
     tasksByUser,
-    "Select a task to view more details or delete:"
-    );
+    "Select a task to view more details:",
+    true // include actions
+  );
 
-    while (!exit) {
-        const menuOption = await inquirer.prompt([menu]);
-        if (menuOption.menu === "Back") {
-            exit = true;
-        } else if (menuOption.menu === "Delete") {
-            await deleteTask(taskController, user);
-            tasksArray = await taskController.getStartedTasks();
-            tasksByUser = getTasksByUser(tasksArray, user);
-            menu.choices = getTaskTitlesMenu(tasksByUser, "Select a task to view more details or delete:").choices;
-        } else {
-            console.log(tasksByUser[menuOption.menu.charAt(0) - 1]);
-        }
+  while (!exit) {
+    const menuOption = await inquirer.prompt([menu]);
+    if (menuOption.menu === "Back") {
+      exit = true;
+    } else {
+      const task = tasksByUser[menuOption.menu.charAt(0) - 1];
+      const actionMenu = {
+        type: "list",
+        name: "action",
+        message: `Selected task: ${task.title}`,
+        choices: ["View details", "Delete task", "Update status", "Back"],
+      };
+
+      const actionOption = await inquirer.prompt([actionMenu]);
+
+      switch (actionOption.action) {
+        case "View details":
+          console.log(task);
+          break;
+        case "Delete task":
+          const ca = await inquirer.prompt([confirmAction]);
+          if (ca.confirm) {
+            await taskController.delete(task.id);
+            tasksByUser.splice(menuOption.menu.charAt(0) - 1, 1);
+            console.log("Task deleted");
+            menu.choices.pop();
+          }
+          break;
+        case "Update status":
+          // POR HACER
+          console.log("Update status selected");
+          break;
+        case "Back":
+          // do nothing
+          break;
+        default:
+          console.log("Invalid action selected");
+          break;
+      }
     }
+  }
 }
 
 //Filter tasks by user:
@@ -165,19 +196,23 @@ function getTasksByUser(tasksArray, user) {
 }
 
 //Get task titles menu:
-function getTaskTitlesMenu(tasks, message) {
-    const choices = tasks.map((task, index) => `${index + 1}. ${task.title}`);
-    choices.push("Back");
-    choices.push("Delete");
+function getTaskTitlesMenu(tasks, message, includeActions = false) {
+  const choices = tasks.map((task, index) => `${index + 1}. ${task.title}`);
+  choices.push("Back");
 
-    const menu = {
-        type: 'list',
-        name: 'menu',
-        message: message,
-        choices: choices
-        };
+  if (includeActions) {
+    choices.push(" ");
+    choices.push("Select an action:");
+  }
 
-    return menu;
+  const menu = {
+    type: "list",
+    name: "menu",
+    message: message,
+    choices: choices,
+  };
+
+  return menu;
 }
 
 module.exports = initProgram;
