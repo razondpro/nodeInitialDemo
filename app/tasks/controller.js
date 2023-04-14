08f6cb609e29aab9f6350e26fc3221f7b2594aa8
Razon Miah <miahrazon@gmail.com>
2023-04-14 13:33:31 +0200

    small comments and deleting unused functions

--- app/tasks/controller.js
+++ app/tasks/controller.js
@@ -15,34 +15,48 @@ class TaskController {
         const t = await this.service.create(task)
         return t
     }
-
-    async retrieve(id){
-        
-    }
-
+    /**
+     * Get all tasks
+     * @returns all tasks
+     */
     async retrieveAll() {
         const t = await this.service.retrieveAll()
         return t.getTasks();
     }
-
+    /**
+     * Updates a task
+     * @param {*} task 
+     */
     async update(task){
         await this.service.update(task)
     }
-
+    /**
+     * deletes a task with given id
+     * @param {*} id 
+     */
     async delete(id){
         await this.service.delete(id)
     }
-
+    /**
+     * Get all pending tasks
+     * @returns pending tasks
+     */
     async getPendingTasks(){
         const tasks = await this.service.retrieveAll();
         return tasks.getPendingTasks();
     }
-
+    /**
+     * Get all started tasks
+     * @returns started tasks
+     */
     async getStartedTasks(){
         const tasks = await this.service.retrieveAll();
         return tasks.getStartedTasks();
     }
-
+    /**
+     * Get all finished tasks
+     * @returns finished tasks
+     */
     async getFinishedTasks(){
         const tasks = await this.service.retrieveAll();
         return tasks.getFinishedTasks();
--- app/tasks/task.list.js
+++ app/tasks/task.list.js
@@ -2,22 +2,38 @@ class TaskList {
     constructor(tasks){
         this.tasks = tasks || [];
     }
+    /**
+     * Get all tasks from the list
+     * @returns tasks
+     */
     getTasks(){
         return this.tasks;
     }
-
+    /**
+     * Add a new task in tasks array
+     * @param {*} task 
+     */
     append(task){
         this.tasks.push(task)
     }
-
+    /**
+     * get all tasks marked as pending
+     * @returns tasks
+     */
     getPendingTasks(){
         return this.tasks.filter(task => task.status === 'pending');
     }
-
+    /**
+     * get all tasks marked as started
+     * @returns tasks
+     */
     getStartedTasks(){
         return this.tasks.filter(task => task.status === 'started');
     }
-
+    /**
+     * get all tasks marked as finished
+     * @returns tasks
+     */
     getFinishedTasks(){
         return this.tasks.filter(task => task.status === 'finished');
     }
